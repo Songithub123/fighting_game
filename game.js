f08f07076a4de681a48bc2edafb35bbe8f4cf343
Songithub123 <ntson2092003@gmail.com>
2023-07-10 23:28:40 +0700

    added functions

--- game.js
+++ game.js
@@ -1,8 +1,28 @@
-let character = function () {
-  let stat = {
+let character = (function () {
+  let stats = {
     atk: 100,
     def: 100,
+    hp: 1000,
     mp: 100,
   };
-  
-};
+  function handleCLick(){
+    console.log("idiot")
+  }
+  function actions (action) {
+    switch (action){
+      case "attack":
+        console.log("You attacked");
+        break;
+      case "defense":
+        console.log("You defense");
+        break;
+      case "special":
+        console.log("You use special ability");
+        break;
+    };
+  }
+  return{
+    stats: stats,
+    actions: actions
+  };
+})();
