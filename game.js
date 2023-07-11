89547f4edb3a6aa9ebbf30c5f3bf8c733c3fd498
Songithub123 <107671963+Songithub123@users.noreply.github.com>
2023-07-11 09:15:24 +0700

    Update game.js

--- game.js
+++ game.js
@@ -33,3 +33,39 @@ let character = (function () {
   };
 })();
 
+const button1 = document.createElement('button');
+button1.setAttribute('type', 'button');
+button1.setAttribute('class', 'action');
+button1.setAttribute('id', 'btn1');
+button1.setAttribute('onclick', "character.actions('attack')");
+button1.innerHTML = 'Attack';
+
+const button2 = document.createElement('button');
+button2.setAttribute('type', 'button');
+button2.setAttribute('class', 'action');
+button2.setAttribute('id', 'bt2');
+button2.setAttribute('onclick', "character.actions('defense')");
+button2.innerHTML = 'Defense';
+
+const button3 = document.createElement('button');
+button3.setAttribute('type', 'button');
+button3.setAttribute('class', 'action');
+button3.setAttribute('id', 'btn3');
+button3.setAttribute('onclick', "character.actions('special')");
+button3.innerHTML = 'Special';
+
+const divControl = document.createElement('div');
+divControl.setAttribute('class', 'control');
+
+divControl.appendChild(button1);
+divControl.appendChild(button2);
+divControl.appendChild(button3);
+
+const divBox = document.createElement('div');
+divBox.setAttribute('class', 'box');
+
+divBox.appendChild(divControl);
+
+document.body.appendChild(divBox);
+
+
